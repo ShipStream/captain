bb07d73c191933d9ce95c86bf753cd4c37df5ace
hariharan-io <hello@hariharan.io>
2024-04-26 01:10:19 +0530

    Improved docker compose compatibility with github actions

--- captain/__tests__/utils/jestSetup.ts
+++ captain/__tests__/utils/jestSetup.ts
@@ -1,8 +1,14 @@
+import { CustomRaceConditionLock } from "../../src/coreUtils.js"
+
+const unhandledRejectionToIgnore = [CustomRaceConditionLock.lockObsolete];
+
 // preserve original setTimeout in case needed to be used when using fake timers
 (global as any).originalSetTimeout = global.setTimeout
 process.on('unhandledRejection', (error, origin) => {
-  console.error(origin, error)
-  fail(error)
+  if (!unhandledRejectionToIgnore?.includes(`${error}`)) {
+    console.error(origin, error)
+    fail(error)  
+  }
 })
 process.on('uncaughtException', (error, origin) => {
   console.error(origin, error)
